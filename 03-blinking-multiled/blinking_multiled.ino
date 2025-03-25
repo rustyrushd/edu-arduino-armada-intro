5790357d4ce3fc84984a3a2a9779b55f2f534614
rustyrushd <204103529+rustyrushd@users.noreply.github.com>
2025-03-25 06:04:15 +0000

    Upload blinking-multiled circuit and code

---
+++ 03-blinking-multiled/blinking_multiled.ino
@@ -0,0 +1,41 @@
+/*
+  This program builds upon the previous single blinking LED.
+  Get that working in both Tinkercad and hardware before attempting this.
+  
+  Then copy the Tinkercad circuit. Add the yellow LED. Again test it.
+  
+  Then add the green and test.
+  
+  The circuit should blink red then yellow then green, each for 1 second.
+*/
+
+
+
+const int RED = 13;
+
+void setup()
+{
+  pinMode(RED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(RED, HIGH);
+  delay(1000);
+  digitalWrite(RED, LOW);
+  delay(1000);
+}
+
+
+/* 
+  Bored? Try:
+  
+  1. Imagine these are traffic lights that cycle every 10 seconds.
+  Change the delay times so that red and green are on for 4 seconds
+  each and yellow for 2 seconds.
+  
+  2. Reversing the lighting order of LEDs,
+  green then yellow then red (in software).
+  
+  3. Also try making the LEDs blink at same time.  
+*/
