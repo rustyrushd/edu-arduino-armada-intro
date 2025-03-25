234d46eb1e07e76870a501d6143352425443b4bd
rustyrushd <204103529+rustyrushd@users.noreply.github.com>
2025-03-25 06:18:59 +0000

    Add green led to blinking_multiled.ino

--- 03-blinking-multiled/blinking_multiled.ino
+++ 03-blinking-multiled/blinking_multiled.ino
@@ -12,11 +12,13 @@
 
 const int RED = 13;
 const int YELLOW = 9;
+const int GREEN = 6;
 
 void setup()
 {
   pinMode(RED, OUTPUT);
   pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
 }
 
 void loop()
@@ -28,6 +30,10 @@ void loop()
   digitalWrite(YELLOW, HIGH);
   delay(1000);
   digitalWrite(YELLOW, LOW);
+  delay(1000);
+  digitalWrite(GREEN, HIGH);
+  delay(1000);
+  digitalWrite(GREEN, LOW);
   delay(1000);
 }
 
